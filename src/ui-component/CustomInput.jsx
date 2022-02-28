e7bfc05623ed954e9257fe2b5c9e18eaa6a9a534
bipinrai054 <bipinrai054@gmail.com>
2022-02-28 18:03:18 +0545

    checkbox initialized

--- src/ui-component/CustomInput.jsx
+++ src/ui-component/CustomInput.jsx
@@ -8,7 +8,7 @@ export function CustomInput({ type, placeholder, label }) {
     return (
         <div>
             {/* eslint-disable-next-line */}
-            <Typography sx={{ mt: 3, mb: 2 }}>
+            <Typography sx={{ mt: 3, mb: 1 }}>
                 <label className="test" htmlFor="input">
                     {label}
                 </label>
@@ -22,12 +22,12 @@ export function CustomInput({ type, placeholder, label }) {
 export function CustomSelect({ label }) {
     return (
         <div>
-            <Typography sx={{ mt: 4, mb: 1 }}>
-                <label className="test" htmlFor="input">
+            <Typography sx={{ mt: 3, mb: 1 }}>
+                <label className="test" htmlFor="select">
                     {label}
                 </label>
             </Typography>
-            <select>
+            <select id="select">
                 <option value="volvo">Volvo</option>
                 <option value="saab">Saab</option>
                 <option value="opel">Opel</option>
@@ -42,6 +42,18 @@ export function CustomTextarea({ placeholder }) {
     return (
         <div>
             <textarea placeholder={placeholder} rows="3" />
+        </div>
+    );
+}
+
+// eslint-disable-next-line
+export function CustomCheckbox({ label, nameType }) {
+    return (
+        <div>
+            <Typography>
+                <label htmlFor="radio">{(label, nameType)}</label>
+            </Typography>
+            <input id="radio" type="radio" name={nameType} />
         </div>
     );
 }
--- src/views/sample-page/index.js
+++ src/views/sample-page/index.js
@@ -1,20 +1,39 @@
 // material-ui
-import { Typography } from '@mui/material';
+// import { Typography } from '@mui/material';
 
 // project imports
-import MainCard from 'ui-component/cards/MainCard';
+// import MainCard from 'ui-component/cards/MainCard';
+
+// mui
+import { Grid, Typography, Paper } from '@mui/material';
+
+// components
+import { CustomCheckbox } from '../../ui-component/CustomInput';
 
 // ==============================|| SAMPLE PAGE ||============================== //
 
 const SamplePage = () => (
-    <MainCard title="Sample Card">
-        <Typography variant="body2">
-            Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif
-            ad minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
-            reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa
-            qui officiate descent molls anim id est labours.
-        </Typography>
-    </MainCard>
+    <Grid container>
+        <Grid item>
+            <Typography variant="h4">Dashboard / Menu / Create</Typography>
+        </Grid>
+
+        <Grid item xs={12} sx={{ height: '100%' }}>
+            <Paper>
+                <Typography variant="h3">Choose Item Type</Typography>
+
+                <Grid container>
+                    <Grid item xs={6}>
+                        <CustomCheckbox type="radio" label="Variant" nameType="item_variant" />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <CustomCheckbox type="radio" label="Non-variant" nameType="item_variant" />
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Grid>
+    </Grid>
 );
 
 export default SamplePage;
