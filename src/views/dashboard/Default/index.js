7aa59bcc31cdb7b01dab1a51bc5e19c67641bc2c
bipinrai054 <bipinrai054@gmail.com>
2022-02-28 13:40:11 +0545

    form input design done

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // material-ui
 import { Grid, Paper, Typography } from '@mui/material';
@@ -18,19 +18,53 @@ import { CustomInput, CustomSelect, CustomTextarea } from '../../../ui-component
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
 const Dashboard = () => {
+    const [step, setStep] = useState(1);
+
     const [isLoading, setLoading] = useState(true);
     useEffect(() => {
         setLoading(false);
     }, []);
 
     return (
-        <Grid container spacing={gridSpacing}>
+        <Grid container spacing={3}>
             <Grid item>
                 <Typography variant="h4">Dashboard / Menu / Create</Typography>
             </Grid>
             <Grid item xs={12}>
-                <Paper sx={{ p: 4 }}>
-                    <CustomTextarea placeholder="Description" />
+                <Paper sx={{ p: 3 }}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <Grid container flexDirection="column" spacing={3}>
+                                <Grid item>
+                                    <CustomSelect />
+                                </Grid>
+                                <Grid item>
+                                    <CustomInput type="text" placeholder="Item Name" />
+                                </Grid>
+                                <Grid item>
+                                    <CustomInput type="number" placeholder="Alert Quantity" />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Grid container flexDirection="column" spacing={3}>
+                                <Grid item>
+                                    <CustomSelect />
+                                </Grid>
+                                <Grid item>
+                                    <CustomInput type="date" />
+                                </Grid>
+                                <Grid item>
+                                    <CustomInput type="file" />
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                    <Grid container sx={{ mt: 5 }}>
+                        <Grid item xs={12}>
+                            <CustomTextarea placeholder="Description" />
+                        </Grid>
+                    </Grid>
                 </Paper>
             </Grid>
         </Grid>
