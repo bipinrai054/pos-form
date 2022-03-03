a38e71ea9265e4c1935795baa429ce9e77b86231
bipinrai054 <bipinrai054@gmail.com>
2022-03-03 11:14:26 +0545

    conditional rendering form implemented

--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable*/
+
 import React from 'react';
 // mui
 import { Grid, Paper, Typography, Button } from '@mui/material';
@@ -5,34 +7,16 @@ import { Grid, Paper, Typography, Button } from '@mui/material';
 // mui-icons
 import { ArrowBack, ArrowForward } from '@mui/icons-material';
 
-// project imports
-// import EarningCard from './EarningCard';
-// import PopularCard from './PopularCard';
-// import TotalOrderLineChartCard from './TotalOrderLineChartCard';
-// import TotalIncomeDarkCard from './TotalIncomeDarkCard';
-// import TotalIncomeLightCard from './TotalIncomeLightCard';
-// import TotalGrowthBarChart from './TotalGrowthBarChart';
-// import { gridSpacing } from 'store/constant';
-
 // new components
 import CustomStepper from '../../../ui-component/CustomStepper';
 import VariantDetails from '../../../components/VariantDetails';
-// import VariantChoice from '../../../components/VariantChoice';
-// import ItemDetails from '../../../components/ItemDetails';
+import VariantChoice from '../../../components/VariantChoice';
+import ItemDetails from '../../../components/ItemDetails';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
-// eslint-disable-next-line
 const Dashboard = () => {
-    // const [step, setStep] = useState(1);
-
-    // const [isLoading, setLoading] = useState(true);
-    // useEffect(() => {
-    //     setLoading(false);
-    // }, []);
-
     const [activeStep, setActiveStep] = React.useState(0);
 
-    // eslint-disable-next-line
     const prevStep = () => {
         if (activeStep <= 0) {
             setActiveStep(0);
@@ -41,7 +25,6 @@ const Dashboard = () => {
         }
     };
 
-    // eslint-disable-next-line
     const nextStep = () => {
         if (activeStep >= 4) {
             setActiveStep(4);
@@ -49,8 +32,6 @@ const Dashboard = () => {
             setActiveStep((currentStep) => currentStep + 1);
         }
     };
-
-    console.log(activeStep);
 
     return (
         <Grid container spacing={3}>
@@ -61,30 +42,38 @@ const Dashboard = () => {
                 <Paper sx={{ p: 3 }}>
                     <Grid sx={{ p: 1, display: 'flex', justifyContent: 'center' }}>
                         <Grid item xs={8}>
-                            <CustomStepper activeStep={activeStep} />
+                            <CustomStepper
+                            //  activeStep={activeStep}
+                            />
                         </Grid>
                     </Grid>
 
-                    {/* item details start */}
-                    {/* <ItemDetails /> */}
-                    {/* item details end */}
-
-                    {/* variant choosing component start */}
-                    {/* <VariantChoice /> */}
-                    {/* varaint choosing component end */}
-
-                    {/* variant details start */}
-                    <VariantDetails />
-                    {/* variant details end */}
+                    {activeStep === 0 ? (
+                        <ItemDetails />
+                    ) : activeStep === 1 ? (
+                        <VariantChoice />
+                    ) : activeStep === 2 ? (
+                        <VariantDetails />
+                    ) : null}
 
                     <Grid container sx={{ mt: 3, display: 'flex', justifyContent: 'space-between' }}>
                         <Grid item>
-                            <Button className="form-btn" variant="outlined" startIcon={<ArrowBack />} onClick={prevStep}>
+                            <Button
+                                className="form-btn"
+                                variant="outlined"
+                                startIcon={<ArrowBack />}
+                                // onClick={prevStep}
+                            >
                                 Previous
                             </Button>
                         </Grid>
                         <Grid item>
-                            <Button className="form-btn" variant="contained" endIcon={<ArrowForward />} onClick={nextStep}>
+                            <Button
+                                className="form-btn"
+                                variant="contained"
+                                endIcon={<ArrowForward />}
+                                // onClick={nextStep}
+                            >
                                 Next
                             </Button>
                         </Grid>
